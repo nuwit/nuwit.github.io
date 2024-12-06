eddecf0ebe5a100935ff1d52d5936ecc51268ed4
esha-pandya0203 <pandya.e@northeastern.edu>
2024-12-05 21:12:26 -0500

    Fixed routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Officers from './pages/Officers';
 import Sponsors from './pages/Sponsors';
