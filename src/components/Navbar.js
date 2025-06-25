4c445cdf62009b6d96ac76a70f9e58ab2cd3006c
slaurence1003 <sabine.a.laurence@gmail.com>
2025-06-25 13:00:02 -0700

    made more mobile friendly!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import '../styles/Navbar.css'; 
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggle = () => setMenuOpen(!menuOpen);
+  const handleLinkClick = () => setMenuOpen(false);
+
   return (
     <nav className='navbar'>
-        <div className='logo'>
+      <div className='logo'>
         <Link to="/">
           <img src={logo} alt='NUWIT logo'/>
         </Link>
-        </div>
-      <ul className='nav-links'>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/officers">Officers</Link></li>
-        <li><Link to="/sponsors">Sponsors</Link></li>
-        <li><Link to="/calendar">Calendar</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+      </div>
+      <button className='hamburger' onClick={handleToggle} aria-label='Toggle menu'>
+        <span className={menuOpen ? 'bar open' : 'bar'}></span>
+        <span className={menuOpen ? 'bar open' : 'bar'}></span>
+        <span className={menuOpen ? 'bar open' : 'bar'}></span>
+      </button>
+      <ul className={`nav-links${menuOpen ? ' open' : ''}`}>
+        <li><Link to="/" onClick={handleLinkClick}>Home</Link></li>
+        <li><Link to="/officers" onClick={handleLinkClick}>Officers</Link></li>
+        <li><Link to="/sponsors" onClick={handleLinkClick}>Sponsors</Link></li>
+        <li><Link to="/calendar" onClick={handleLinkClick}>Calendar</Link></li>
+        <li><Link to="/contact" onClick={handleLinkClick}>Contact</Link></li>
       </ul>
     </nav>
   );
